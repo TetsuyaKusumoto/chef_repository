bb0bc2a0b2646d5a4d73277fea44fe2dbb0fb484
Tetsuya Kusumoto <tetsuya.kusumoto@gmail.com>
2016-10-31 09:02:42 +0900

    modify rbenv install path in rbenv recipe

--- site-cookbooks/ruby-env/recipes/default.rb
+++ site-cookbooks/ruby-env/recipes/default.rb
@@ -16,12 +16,29 @@
 end
 
 # rbenv
-git "/home/#{node['ruby-env']['user']}/.rbenv" do
+#git "/home/#{node['ruby-env']['user']}/.rbenv" do
+#  repository node['ruby-env']['rbenv_url']
+#  action :sync
+#  user  node['ruby-env']['user']
+#  group node['ruby-env']['group']
+#end
+
+git "#{node[ruby-env][install_path]}/.rbenv" do
   repository node['ruby-env']['rbenv_url']
   action :sync
   user  node['ruby-env']['user']
   group node['ruby-env']['group']
 end
+
+
+#template ".bash_profile" do
+#  source ".bash_profile.erb"
+#  path   "/home/#{node['ruby-env']['user']}/.bash_profile"
+#  mode   0655
+#  owner  node['ruby-env']['user']
+#  group  node['ruby-env']['group']
+#  not_if "grep rbenv ~/.bash_profile", environment: { :'HOME' => "/home/#{node['ruby-env']['user']}" }
+#end
 
 template ".bash_profile" do
   source ".bash_profile.erb"
@@ -33,14 +50,14 @@ template ".bash_profile" do
 end
 
 # ruby
-directory "/home/#{node['ruby-env']['user']}/.rbenv/plugins" do
+directory "#{node['ruby-env']['install_path']}/.rbenv/plugins" do
   mode   0755
   owner  node['ruby-env']['user']
   group  node['ruby-env']['group']
   action :create
 end
 
-git "/home/#{node['ruby-env']['user']}/.rbenv/plugins/ruby-build" do
+git "#{node['ruby-env']['install_path']}/.rbenv/plugins/ruby-build" do
   repository node['ruby-env']['ruby-build_url']
   action :sync
   user   node['ruby-env']['user']
@@ -49,16 +66,16 @@ end
 
 # install ruby
 execute "rbenv install #{node['ruby-env']['version']}" do
-  command "/home/#{node['ruby-env']['user']}/.rbenv/bin/rbenv install #{node['ruby-env']['version']}"
+  command "#{node['ruby-env']['install_path']}/.rbenv/bin/rbenv install #{node['ruby-env']['version']}"
   user   node['ruby-env']['user']
   group  node['ruby-env']['group']
   environment 'HOME' => "/home/#{node['ruby-env']['user']}"
-  not_if { File.exists?("/home/#{node['ruby-env']['user']}/.rbenv/versions/#{node['ruby-env']['version']}") }
+  not_if { File.exists?("#{node['ruby-env']['install_path']}/.rbenv/versions/#{node['ruby-env']['version']}") }
 end
 
 # set rbenv global
 execute "rbenv global #{node['ruby-env']['version']}" do
-  command "/home/#{node['ruby-env']['user']}/.rbenv/bin/rbenv global #{node['ruby-env']['version']}"
+  command "#{node['ruby-env']['install_path']}/.rbenv/bin/rbenv global #{node['ruby-env']['version']}"
   user   node['ruby-env']['user']
   group  node['ruby-env']['group']
   environment 'HOME' => "/home/#{node['ruby-env']['user']}"
@@ -67,11 +84,11 @@ end
 # install rbenv-rehash and bundler gem
 %w{rbenv-rehash bundler}.each do |gem_name|
   execute "gem install #{gem_name}" do
-    command "/home/#{node['ruby-env']['user']}/.rbenv/shims/gem install #{gem_name}"
+    command "#{node['ruby-env']['install_path']}/.rbenv/shims/gem install #{gem_name}"
     user   node['ruby-env']['user']
     group  node['ruby-env']['group']
     environment 'HOME' => "/home/#{node['ruby-env']['user']}"
     # すでにインストールリストになければ
-    not_if "/home/#{node['ruby-env']['user']}/.rbenv/shims/gem list | grep #{gem_name}"
+    not_if "#{node['ruby-env']['install_path']}/.rbenv/shims/gem list | grep #{gem_name}"
   end
 end
